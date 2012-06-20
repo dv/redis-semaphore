b7deb33d340355f257dc3e2699193e8993613521
Petteri Räty <petsku@petteriraty.eu>
2012-06-20 15:57:39 +0300

    Remove unused require for logger in specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,6 @@ require 'rubygems'
 require 'bundler/setup'
 Bundler.require(:development)
 
-require 'logger'
-
 $TESTING=true
 $:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
 require 'redis/semaphore'
